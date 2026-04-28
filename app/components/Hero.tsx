e752987fee6f6fe67b3d4367209b1fa66192f137
speer987 <saarah.peer15@gmail.com>
2026-04-27 18:16:33 -0700

    updating site hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,21 +2,19 @@ export default function Hero() {
   return (
     <div>
       <div className="divide-x divide-[#5b422a] flex flex-row text-[#5b422a] bg-[#fbf6ee]">
-        <div className="w-1/2 flex flex-col gap-1 p-25 font-sans flex-none">
+        <div className="w-1/2 flex flex-col gap-1 p-25 font-sans justify-center">
           <p className="text-2xl">Hello, I'm</p>
           <p className="text-8xl font-serif">Saarah Peer</p>
           <p className="text-2xl">I'm passionate about</p>
-          <div className="text-md font-barlow flex gap-2 mt-2 font-medium">
-            <button className="cursor-pointer p-3 border-2 uppercase text-[#fbf6ee] bg-[#A8794D] hover:bg-[#5b422a] border-[#e8d4b6] transition duration-300 ease-in-out">
+          <ul className="text-xl font-medium font-barlow flex gap-2 mt-2 font-md uppercase">
+            <li className="bg-white px-1 border border-[#f4e4cf]">
               Software Development
-            </button>
-            <button className="cursor-pointer p-3 border-2 uppercase text-[#fbf6ee] bg-[#A8794D] hover:bg-[#5b422a] border-[#e8d4b6] transition duration-300 ease-in-out">
+            </li>
+            <li className="bg-white px-1 border border-[#f4e4cf]">
               Data Analytics
-            </button>
-            <button className="cursor-pointer p-3 border-2 uppercase text-[#fbf6ee] bg-[#A8794D] hover:bg-[#5b422a] border-[#e8d4b6] transition duration-300 ease-in-out">
-              UI Design
-            </button>
-          </div>
+            </li>
+            <li className="bg-white px-1 border border-[#f4e4cf]">Design</li>
+          </ul>
         </div>
         <div className="w-1/2 flex flex-col gap-1 p-25">
           <div className="text-5xl font-serif">About Me</div>
