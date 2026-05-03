f4032cee588ac947e96faa246792f7d8f4379a7f
speer987 <saarah.peer15@gmail.com>
2026-05-03 11:55:37 -0700

    adding updated files for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: '/',
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
