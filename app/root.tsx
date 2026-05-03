526e0ded6cb1ceba0f47bda2e4a091921eb6e280
speer987 <saarah.peer15@gmail.com>
2026-05-03 13:04:12 -0700

    updating favicon

--- app/root.tsx
+++ app/root.tsx
@@ -30,6 +30,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Saarah Peer's Portfolio</title>
+
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <Meta />
         <Links />
       </head>
